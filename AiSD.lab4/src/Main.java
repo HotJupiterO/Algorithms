1fcaaed40c5a1248655e58e9a7399c59e95c9bde
HotJupiter0 <jaroslawrepecki@gmail.com>
2019-03-23 12:01:42 +0100

    some chenges

---
+++ AiSD.lab4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Test test = new Test();
+        System.out.println(test.sum(2, 2, 3, 4, 5));
+        Object tmp1 = null;
+        test.test(tmp1);
+    }
+}
